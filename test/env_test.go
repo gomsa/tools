c019f36da7c3c843960f1cdd5d54ce185c68196d
bigrocs <bigrocs@qq.com>
2019-01-21 11:04:54 +0800

    增加默认获取系统参数附加默认值

---
+++ test/env_test.go
@@ -0,0 +1,14 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gomsa/tools/env"
+)
+
+func TestEnv(t *testing.T) {
+	val := env.Getenv("DB_DRIVER", "123456")
+	if val != "123456" {
+		t.Errorf("Database connection failed, %v!", val)
+	}
+}
