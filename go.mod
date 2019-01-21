f6eaac8767dc41d137ed24989250ce6ba1b42376
bigrocs <bigrocs@qq.com>
2019-01-21 10:14:45 +0800

    增加 xorm 管理

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/gomsa/tools
+
+require (
+	github.com/alexbrainman/odbc v0.0.0-20190102080306-cf37ce290779
+	github.com/go-sql-driver/mysql v1.4.1 // indirect
+	github.com/go-xorm/xorm v0.7.1
+	github.com/jinzhu/gorm v1.9.2
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
+)
---
+++ test/xorm_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"testing"
+
+	xorm "github.com/gomsa/tools/xorm"
+)
+
+func TestXorm(t *testing.T) {
+	_, err := xorm.Connection(&xorm.Config{
+		Driver: "odbc",
+		// Host 主机地址
+		Host: "192.168.1.26",
+		// Port 主机端口
+		Port: "1433",
+		// User 用户名
+		User: "sa",
+		// Password 密码
+		Password: "",
+		// DbName 数据库名称
+		DbName: "bvbv01",
+		// Charset 数据库编码
+		Charset: "gbk",
+	})
+	if err != nil {
+		t.Errorf("Database connection failed, %v!", err)
+	}
+}
