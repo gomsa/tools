92dd46d9322939180a765889c09d1849a1e343e2
bigrocs <bigrocs@qq.com>
2019-06-06 15:01:19 +0800

    自动提交 git 代码

--- config/config_test.go
+++ config/config_test.go
@@ -6,19 +6,14 @@ import (
 )
 
 func TestConfig(t *testing.T) {
-	conf := &Config{}
-	conf.LoadFile("config.yaml")
-	App := conf.GetApp()
-	Version := conf.GetVersion()
-	Permissions := conf.GetPermissions()
 	fmt.Println(
-		App,
-		Version,
-		Permissions[0].Service,
-		Permissions[0].Method,
-		Permissions[0].Auth,
-		Permissions[0].Policy,
-		Permissions[0].DisplayName,
-		Permissions[0].Description,
+		Conf.App,
+		Conf.Version,
+		Conf.Permissions[0].Service,
+		Conf.Permissions[0].Method,
+		Conf.Permissions[0].Auth,
+		Conf.Permissions[0].Policy,
+		Conf.Permissions[0].DisplayName,
+		Conf.Permissions[0].Description,
 	)
 }
