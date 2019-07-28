d6f50b9a663c2177a8d6bce254d12bc0879d4916
bigrocs <bigrocs@qq.com>
2019-07-28 11:01:43 +0800

    自动提交 git 代码

---
+++ uitl/page.go
@@ -0,0 +1,15 @@
+package uitl
+
+func Page(Limit,Page int64) (limit, offset int64) {
+	if Limit > 0 {
+		limit = Limit
+	} else {
+		limit = 10
+	}
+	if Page > 1 {
+		offset = (Page - 1) * limit
+	} else {
+		offset = -1
+	}
+	return limit, offset
+}
