51a81576c569a5662dbfc6dd0ae0762260a92347
bigrocs <bigrocs@qq.com>
2019-08-03 09:06:05 +0800

    自动提交 git 代码

---
+++ uitl/sys.go
@@ -0,0 +1,12 @@
+package uitl
+
+import "reflect"
+
+//IsNil 是否为空指针
+func IsNil(i interface{}) bool {
+	vi := reflect.ValueOf(i)
+	if vi.Kind() == reflect.Ptr {
+		return vi.IsNil()
+	}
+	return false
+}
