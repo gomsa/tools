e6129666b8ac49a2408d1a99470fafde63d9298e
bigrocs <bigrocs@qq.com>
2019-10-23 17:05:30 +0800

    自动提交 git 代码

--- uitl/where.go
+++ uitl/where.go
@@ -3,20 +3,27 @@ package uitl
 import (
 	"strings"
 	"time"
+
+	"github.com/go-xorm/xorm"
 )
 
 // TimeLayout 转换字符
 const TimeLayout = "2006-01-02 15:04:05"
 
-// Where 条件查询
-func Where(value string) (param string, val interface{}) {
-	split := strings.Split(value, "|")
-	param = split[0] + split[1] + `?`
-	switch split[2] {
-	case `string`:
-		val = split[3]
-	case `time`:
-		val, _ = time.ParseInLocation(TimeLayout, split[3], time.Local)
+// XormWhere Xorm 条件查询
+func XormWhere(engine *xorm.Engine, value string) *xorm.Engine {
+	where := strings.Split(value, ",")
+	for _, whe := range where {
+		split := strings.Split(whe, "|")
+		var val interface{}
+		param := split[0] + split[1] + `?`
+		switch split[2] {
+		case `string`:
+			val = split[3]
+		case `time`:
+			val, _ = time.ParseInLocation(TimeLayout, split[3], time.Local)
+		}
+		engine.Where(param, val)
 	}
-	return param, val
+	return engine
 }
